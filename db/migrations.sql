b03987b1f8d443066cc59f4139b814d76f6269c1
Adam Wilson <adamwilson1234@gmail.com>
2015-12-07 15:08:01 -0600

    updated migrations and seed files to sql for current musical_memories app.

--- db/migrations.sql
+++ db/migrations.sql
@@ -1,9 +1,10 @@
 # create a database
 # connect
-# create a items table
-# items contain name (varchar(255)) and description (varchar(255))
+# create an items table
 
-CREATE DATABASE shopping_list;
-\c shopping_list
-CREATE TABLE items (id SERIAL PRIMARY KEY, name VARCHAR(255), quantity INTEGER, whenithappened TIMESTAMP);
-CREATE TABLE validations (id SERIAL PRIMARY KEY, user_name VARCHAR(255), email VARCHAR(255), pass_digest VARCHAR(255), is_admin BOOLEAN);
+CREATE DATABASE musical_memories;
+\c musical_memories
+
+CREATE TABLE users (id SERIAL PRIMARY KEY, user_name VARCHAR(255), email VARCHAR(255), password VARCHAR(255) );
+
+CREATE TABLE memories (id SERIAL PRIMARY KEY, album VARCHAR(255), artist VARCHAR(255), year INTEGER, user_id INTEGER, specific_song VARCHAR(255), setting VARCHAR(255), who VARCHAR(255), notes TEXT );
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,12 @@
 # insert a few rows into your table
 
-INSERT INTO items (name, quantity) VALUES ('Orange juice, in gallons', 2), ('Honey Bunches of Oats, in boxes', 4), ('Socks, in pairs', 6);
-INSERT INTO validations (user_name, email, pass_digest, is_admin) VALUES ('root', 'email@provider.com', 'master', TRUE);
+INSERT INTO users (user_name, password) VALUES ('adam', '1234'), ('renee', '1234');
+
+INSERT INTO memories (album, artist, user_id, notes) VALUES ('ten', 'pearl jam', 1, 'Macalester freshman year'), ('album', 'mary chapin carpenter', 2, 'Life and love.');
+
+
+# TO SELECT ONLY ENTRIES FOR PARTICULAR users
+# IE THE ONE FOR THE CURRENT SESSION
+-- SELECT album, artist, year, who, specific_song, setting, notes
+-- 	FROM memories
+-- 	WHERE user_id = 1;
