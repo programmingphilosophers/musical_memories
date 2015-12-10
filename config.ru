ad5a5a4cf940660927d695b08e3dec4241cbf583
Adam Wilson <adamwilson1234@gmail.com>
2015-12-10 09:40:13 -0600

    readme half done...

--- config.ru
+++ config.ru
@@ -5,8 +5,8 @@ require('./controllers/application')
 require('./controllers/memory')
 require('./controllers/account')
 
-require('./models/memory')
-require('./models/account')
+require('./models/Memory')
+require('./models/Account')
 
 map('/') { run AccountController }
 map('/memories') { run MemoryController } # /memories/name_of_route
--- db/migrations.sql
+++ db/migrations.sql
@@ -3,6 +3,6 @@ CREATE DATABASE musical_memories;
 
 \c musical_memories
 
-CREATE TABLE accounts (id SERIAL PRIMARY KEY, user_name VARCHAR(255), email VARCHAR(255), password VARCHAR(255) );
+CREATE TABLE accounts (id SERIAL PRIMARY KEY, user_name VARCHAR(255), email VARCHAR(255), password_digest VARCHAR(255) );
 
 CREATE TABLE memories (id SERIAL PRIMARY KEY, album VARCHAR(255), artist VARCHAR(255), year INTEGER, user_id INTEGER, specific_song VARCHAR(255), setting VARCHAR(255), notes TEXT, img_url TEXT);
